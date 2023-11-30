f8efd68b4e2ded119ecd2c595e7471784e995307
Egor Dudkin <krusalovpro@gmail.com>
2023-11-30 21:59:25 +0400

    up

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,26 +1,41 @@
 import React, { useEffect, useRef, useState } from 'react';
 import GerbLogo from '../assets/gerb.png';
 import { useNavigate } from 'react-router-dom';
+import { getCookieToken } from '../utils/utils';
 
 function Header() {
     const navigate = useNavigate();
+    const [admin, setAdmin] = useState(false);
+
+    const url = window.location.pathname;
+    
+    useEffect(()=>{
+        setAdmin(getCookieToken() ? true : false);
+    },[])
+
     return (
         <>
             <header className='w-full absolute top-0 left-0 py-[40px] flex flex-row justify-center items-center z-[1000] px-5'>
                 <img className='absolute left-[5%] cursor-pointer' onClick={() => {
                     navigate("/")
                 }} width={64} height={64} src={GerbLogo} />
-                <a className='text-black mx-5 font-[600] font-[Montserrat] cursor-pointer bg-[#D2F881] px-8 py-2 rounded-3xl'
+                <a className={`text-black mx-5 font-[600] font-[Montserrat] cursor-pointer px-8 py-2 rounded-3xl ${url == '/' ? 'bg-[#D2F881]' : ''}`}
                     onClick={() => {
                         navigate("/")
                     }}>
                     Регион
                 </a>
-                <a className='text-black mx-5 px-8 py-2 font-[600] font-[Montserrat] cursor-pointer'>
-                    Маршруты
+                <a className={`text-black mx-5 font-[600] font-[Montserrat] cursor-pointer px-8 py-2 rounded-3xl ${url == '/routes' ? 'bg-[#D2F881]' : ''}`}>
+                    Маршрут
                 </a>
-                <a className='text-black mx-5 px-8 py-2 font-[600] font-[Montserrat] cursor-pointer'>
-                    Туры
+                <a className={`text-black mx-5 font-[600] font-[Montserrat] cursor-pointer px-8 py-2 rounded-3xl ${url == '/place' ? 'bg-[#D2F881]' : ''}`}>
+                    Точка
+                </a>
+                <a className={`text-black mx-5 font-[600] font-[Montserrat] cursor-pointer px-8 py-2 rounded-3xl ${url == '/admin' ? 'bg-[#D2F881]' : ''}`}
+                onClick={() => {
+                    navigate("/admin")
+                }}>
+                    Админ
                 </a>
             </header>
             <div className='h-[120px] mb-[5em]' />
--- front/src/pages/Admin.tsx
+++ front/src/pages/Admin.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Login from '../components/Login';
 import { getCookieToken } from '../utils/utils';
+import { YMaps, Map, Placemark, SearchControl, Clusterer } from '@pbe/react-yandex-maps';
+import { YMapsApi } from '@pbe/react-yandex-maps/typings/util/typing';
 
 function Admin() {
     const [formData, setFormData] = useState({
@@ -17,6 +19,9 @@ function Admin() {
         walk: false,
         time: 0,
     });
+
+    const [point, setPoint] = useState();
+    const map = useRef<any>(null);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value, type, checked } = e.target as any;
@@ -35,6 +40,15 @@ function Admin() {
             }));
         }
     };
+
+    function loadMap(yamap: YMapsApi) {
+        console.log(yamap.map?.GeoObjects)
+        // yamap.map.GeoObjects.events.add("click", (e: any) => {
+        //     console.log(e)
+        //     var coords = e.get('coords');
+        //     alert(coords.join(', '));
+        // });
+    }
 
     const handleSubmit = async () => {
         const cookieToken = getCookieToken();
@@ -56,7 +70,7 @@ function Admin() {
                     }
                 });
 
-                const response = await fetch('/add', {
+                const response = await fetch('http://127.0.0.1:5000/add', {
                     method: 'POST',
                     headers: {
                         Authorization: `Bearer ${token}`,
@@ -78,46 +92,98 @@ function Admin() {
     return (
         <Login>
             <div className='px-[5%] w-full h-full'>
-                <div className="flex flex-col w-[30%]">
-                    <div className="mb-6">
-                        <label
-                            htmlFor="default-input"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Название
-                        </label>
-                        <input
-                            type="text"
-                            id="default-input"
-                            value={formData.name}
-                            onChange={handleInputChange}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    </div>
+                <div className="flex flex-row w-[100%] justify-between">
+                    <div className='flex flex-col w-[50%]'>
+                        <div className="mb-6">
+                            <label
+                                htmlFor="default-input"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                Название
+                            </label>
+                            <input
+                                type="text"
+                                name='name'
+                                id="default-input"
+                                value={formData.name}
+                                onChange={handleInputChange}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        </div>
 
-                    <div className="mb-6">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Краткое описание</label>
-                        <textarea id="message"
-                            value={formData.cardDescription} onChange={handleInputChange}
-                            name='cardDescription'
-                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Гора стрельная..."></textarea>
-                    </div>
+                        <div className="mb-6">
+                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Краткое описание</label>
+                            <textarea id="message"
+                                value={formData.cardDescription} onChange={handleInputChange}
+                                name='cardDescription'
+                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Гора стрельная..."></textarea>
+                        </div>
 
-                    <div className="mb-6">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Описание</label>
-                        <textarea id="message"
-                            value={formData.description} onChange={handleInputChange}
-                            name="description"
-                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Гора стрельная..."></textarea>
-                    </div>
+                        <div className="mb-6">
+                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Описание</label>
+                            <textarea id="message"
+                                value={formData.description} onChange={handleInputChange}
+                                name="description"
+                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Гора стрельная..."></textarea>
+                        </div>
 
-                    <div className="mb-6">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload file</label>
-                        <input
-                            multiple onChange={handleImageChange}
-                            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                            aria-describedby="file_input_help" id="file_input" type="file" />
-                        <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+                        <div className="mb-6">
+                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Город</label>
+                            <input
+                                type="text"
+                                name='city'
+                                id="default-input"
+                                value={formData.city}
+                                onChange={handleInputChange}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        </div>
+
+                        <div className="mb-6">
+                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Локация (местность где находится)</label>
+                            <input
+                                type="text"
+                                name='location'
+                                id="default-input"
+                                value={formData.location}
+                                onChange={handleInputChange}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        </div>
+
+
+                        <div className="mb-6">
+                            <a className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Категории</a>
+
+                        </div>
+
+                        <div className="mb-6">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload file</label>
+                            <input
+                                multiple onChange={handleImageChange}
+                                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                aria-describedby="file_input_help" id="file_input" type="file" />
+                            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+                        </div>
+                        <button className='bg-[#FEEFD7] px-10 py-5 rounded-2xl font-medium mt-auto w-full' onClick={handleSubmit}>Добавить</button>
                     </div>
-                    <button className='bg-[#FEEFD7] px-10 py-5 rounded-2xl font-medium mt-auto w-full' onClick={handleSubmit}>Добавить</button>
+                    <YMaps>
+                        <section className='w-1/2 h-inherit px-[5%]'>
+                            <Map
+                                className='w-full h-full'
+                                onLoad={loadMap}
+                                instanceRef={map}
+                                defaultState={{ center: [53.195876, 50.100186], zoom: 9 }}>
+                                <Clusterer
+                                    options={{
+                                        preset: 'islands#invertedVioletClusterIcons',
+                                        groupByCoordinates: false,
+                                    }}
+                                >
+                                    {point &&
+                                        <Placemark geometry={point} onClick={() => alert('Hello!!!')} />
+                                    }
+                                </Clusterer>
+                            </Map>
+                        </section>
+                    </YMaps>
+
                 </div>
 
             </div>
