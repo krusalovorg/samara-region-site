7fde8ba848d30ecb6884c952bcf49d20915641e5
Misha <134804106+cheked@users.noreply.github.com>
2023-12-03 14:55:17 +0400

    Update main.py
    create function edit, add categories

--- back/main.py
+++ back/main.py
@@ -1,6 +1,5 @@
 import pymysql
 import requests
-
 from config import host, user, password, db_name
 from flask import Flask, jsonify, request, send_file
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
@@ -100,14 +99,21 @@ def super_print(table_name, category=None):
         return result
 
 
-# delete something
+# delete something from table
 def delete_place(id, table_name):
     with connection.cursor() as cursor:
         delete_query = f"DELETE FROM `{table_name}` WHERE id = {id};"
         cursor.execute(delete_query)
         connection.commit()
 
+#edit something from table
+def edit_table(table_name, options, values, id):
+    for i in range(len(options)):
+        edit_query = f"UPDATE {table_name} SET {options[i]} = '{values[i]}' WHERE id = {id};"
+        cursor.execute(edit_query)
+        connection.commit()
 
+#get something from table by id
 def get_place_details_id(place_id, table_name):
     with connection.cursor() as cursor:
         select_query = f"SELECT * FROM {table_name} WHERE id = %s"
@@ -115,7 +121,7 @@ def get_place_details_id(place_id, table_name):
         place_details = cursor.fetchone()
         return place_details
 
-
+#get something from table by name
 def get_place_details_name(place_name, table_name):
     with connection.cursor() as cursor:
         select_query = f"SELECT * FROM {table_name} WHERE name = %s"
@@ -123,7 +129,7 @@ def get_place_details_name(place_name, table_name):
         place_details = cursor.fetchone()
         return place_details
 
-
+#login as admin
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get('username')
@@ -136,7 +142,7 @@ def login():
     else:
         return jsonify({"msg": "Неверный логин или пароль"}), 401
 
-
+#return places
 @app.route('/places')
 def points_return():
     category = request.args.get('category')
@@ -145,27 +151,32 @@ def points_return():
     else:
         return jsonify(super_print('places'))
 
-
+#send routes
 @app.route('/routes')
 def routes_return():
     return jsonify(super_print('routes'))
 
-
+#send category
 @app.route('/category')
 def category_return():
     return jsonify(super_print('category'))
 
-
+#send image
 @app.route('/image/<image_name>', methods=['GET'])
 def send_image(image_name):
     image_path = os.path.join(app.root_path, 'images', image_name)
     return send_file(image_path, as_attachment=True)
 
-
+#add something in table
 @app.route('/add', methods=['POST'])
 # @jwt_required()
 def add_places():
     data = request.form.to_dict()
+    arr = [int(x) for x in data['category'].split(',')]
+    categories = []
+    for number in arr:
+          categories.append(get_place_details_id(number,'category'))
+    data['category']=f'{categories}'
 
     if request.files.get("image", False):
         image = request.files['image']
@@ -178,10 +189,11 @@ def add_places():
     if data.get('walk', False):
         data['walk'] = data['walk'] == 'true'
     print('data', data)
+
     fill_table(connection, data.get("type"), data)
     return jsonify({"success": True}), 200
 
-
+#get somethin by id
 @app.route('/get_details_id', methods=['GET'])
 def return_details_by_id():
     id = request.args.get('id')
@@ -201,7 +213,7 @@ def return_details_by_id():
     else:
         return "Details not found"
 
-
+#get something by name
 @app.route('/get_details_name', methods=['GET'])
 def return_place_by_name():
     id = request.args.get('id')
@@ -220,23 +232,33 @@ def return_place_by_name():
     else:
         return "Place not found"
 
-
+#delete something
 @app.route('/delete', methods=['GET'])
 def delete_by_id():
     id = request.args.get('id')
     table = request.args.get('table_name')
     delete_place(id, table)
 
+#edit something
+@app.route('/edit', methods=['GET'])
+def edit_by_id():
+    id = request.args.get('id')
+    table = request.args.get('table_name')
+    options = request.args.get('options')
+    values = request.args.get('values')
+    edit_table(table, options, values, id)
+
 
 # start code
 if __name__ == "__main__":
     tables = ['places', 'routes', 'category']
-    # data = ("Example Place", "Short description", "Long description", "Example category", "example.jpg", "12.3456, -78.9101", 5, 100, "Example City", "Example Location", True, 60)
+    #data = ("Example Place", "Short description", "Long description", "1,2", "example.jpg", "12.3456, -78.9101", 5, 100, "Example City", "Example Location", True, 60)
+    #fill_table(connection,'places',data)
+    #print(super_print('places'))
     for table in tables:
         cursor = connection.cursor()
         cursor.execute("SHOW TABLES LIKE %s", (table,))
         result = cursor.fetchone()
-
         if not result:
             if table == 'places':
                 create_table_points()
@@ -244,5 +266,6 @@ if __name__ == "__main__":
                 create_table_routes()
             elif table == 'category':
                 create_table_category()
+
     print(super_print('places'))
     app.run()
